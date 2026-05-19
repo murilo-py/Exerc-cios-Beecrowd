da1eedb85356583c8d99f5f04ba41f931221596b
Murilo Prado <108947811+murilo-py@users.noreply.github.com>
2026-05-19 19:42:23 -0400

    Update bee2881.py
    Versão final, corrigindo todos os bugs e depois de quebrar muito minha cabeça que serviu de aprendizado para conseguir.

--- Exercicios/bee2881.py
+++ Exercicios/bee2881.py
@@ -12,45 +12,34 @@ Saída
 Seu programa deve imprimir uma única linha, contendo o nome do aluno que deve receber o bônus.
 '''
 
-n, k = map(int, input().split()) #n sera a quantidade de vezes que vamos contar, e k a posição na lista da chamada.
+n_total, k = map(int, input().split())
 
 lista_chamada = list()
 
-for al in range(n): #coleta os nomes dos alunos
+for al in range(n_total): #coleta os nomes dos alunos
     aluno = input()
     lista_chamada.append(aluno)
 
 alpabeto = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 for c in range(len(lista_chamada) * 2):
-    for n in range(len(lista_chamada)): #ordena os nomes em ordem alpabetica
+    for n in range(0, len(lista_chamada) - 1): #ordena os nomes em ordem alpabetica
 
-        nome = lista_chamada[n]
+        nome = lista_chamada[n] # Coloca os nomes e seus respectivos indices.
+        i_nome = n 
+       
+        proximo_n = lista_chamada[n + 1]
+        i_prox = n + 1
 
-        if n != lista_chamada.index(lista_chamada[-1]): #se o ultimo da chamada n for o ultimo ele coloca o proximo como o nome mais 1
-            proximo_n = lista_chamada[n + 1]
-        else: #senão ele coloca como o proximo sendo o ultimo
-            proximo_n = lista_chamada[-1]
-        
-        #print(nome, proximo_n)
-
-        i_nome = lista_chamada.index(nome) #pega os inidices na chamada do nome e do depois do nome
-        i_prox = lista_chamada.index(proximo_n)
-
-        if (alpabeto.index(nome[0]) > alpabeto.index(proximo_n[0]) or alpabeto.index(proximo_n[0]) < alpabeto.index(nome[0])): 
-            lista_chamada.pop(i_nome)
+        if (alpabeto.index(nome[0]) > alpabeto.index(proximo_n[0])): 
+            
+            lista_chamada.pop(i_prox)
             lista_chamada.insert(i_nome, proximo_n)
 
+        
+        elif (alpabeto.index(nome[0]) == alpabeto.index(proximo_n[0]) and alpabeto.index(nome[1]) > alpabeto.index(proximo_n[1])): 
+            
             lista_chamada.pop(i_prox)
-            lista_chamada.insert(i_prox, nome)
-
-        elif (alpabeto.index(nome[1]) > alpabeto.index(proximo_n[1]) or alpabeto.index(proximo_n[1]) < alpabeto.index(nome[1])): 
-            lista_chamada.pop(i_nome)
             lista_chamada.insert(i_nome, proximo_n)
 
-            lista_chamada.pop(i_prox)
-            lista_chamada.insert(i_prox, nome)
-        
-
-        #print(lista_chamada,n)
 print(lista_chamada[k-1])
